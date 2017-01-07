471a1da441739f06db41a2406a1e6a41c5927c74
Didoutheripper <n_daifallah@esi.dz>
2017-01-07 17:54:20 +0100

    compoenent names must start with a cap.

--- scripts/main_comp.js
+++ scripts/main_comp.js
@@ -1,1 +1,13 @@
-alert('DONE');
+var MyComponent = React.createClass({
+    render: function(){
+        return (
+            <h2>Hello, world!</h2>
+        );
+    }
+});
+
+var MyButton = React.createClass({
+	render: function(){
+		return <a href="#" class="btn btn-sucess">Buttio</a>
+	}
+});
